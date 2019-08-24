7e8224a6022fdaa70ad87fc8fda9756844ba4a00
Sam Bley <sam.bley@lcecorp.com>
2019-08-24 10:51:15 -0500

    refactor and add new packages

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -4,23 +4,23 @@ import { shallow } from 'enzyme';
 import App from '.';
 
 const wrapper = props => (
-    shallow(<App { ...props } />)
+  shallow(<App { ...props } />)
 );
 
 describe('App', () => {
-    test('renders header with correct text', () => {
-        const app = wrapper({});
-        const header = app.find('h1.header');
-        expect(header.text()).toEqual('This page is currently under construction.');
-    });
-    test('renders img tag with appropriate alt text', () => {
-        const app = wrapper({});
-        const gif = app.find('img.pikachu');
-        expect(gif.prop('alt')).toEqual('animated under construction sign');
-    });
-    test('renders img tag with pikachuConstruction.gif', () => {
-        const app = wrapper({});
-        const gif = app.find('img.pikachu');
-        expect(gif.prop('src')).toEqual('https://media.giphy.com/media/S5JSwmQYHOGMo/giphy.gif');
-    });
+  test('renders header with correct text', () => {
+    const app = wrapper({});
+    const header = app.find('h1.header');
+    expect(header.text()).toEqual('This page is currently under construction.');
+  });
+  test('renders img tag with appropriate alt text', () => {
+    const app = wrapper({});
+    const gif = app.find('img.pikachu');
+    expect(gif.prop('alt')).toEqual('animated under construction sign');
+  });
+  test('renders img tag with pikachuConstruction.gif', () => {
+    const app = wrapper({});
+    const gif = app.find('img.pikachu');
+    expect(gif.prop('src')).toEqual('https://media.giphy.com/media/S5JSwmQYHOGMo/giphy.gif');
+  });
 });
