c3a61251799037445fa5bbabbeb23d6a7e149599
Bley <samibanley@gmail.com>
2019-08-25 16:09:10 -0500

    WIP About Me

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,57 +1,89 @@
 import React from 'react';
 import {
   Paper,
-  Avatar,
   Typography,
-  Grid
+  Grid,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent
 } from '@material-ui/core';
 import { makeStyles, styled } from '@material-ui/styles';
 import colorPalette from '../../utils/colorPalette';
+import { socialMedia, interests } from '../../utils/constants';
 
 const styles = makeStyles({
-  section: {
+  grid: {
+    width: '100vw',
+    height: '100vh',
     display: 'flex',
     justifyContent: 'center',
-    height: 'relative',
-    width: '90%',
-    borderRadius: 4,
-    backgroundColor: colorPalette.WHITE,
-    position: 'relative',
-    margin: 30
+    alignItems: 'center',
+    flexDirection: 'row'
   },
-  avatar: {
-    margin: '1rem',
-    width: '15rem',
-    height: '15rem',
-    borderRadius: 10
+  section: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    borderRadius: 4,
+    backgroundColor: colorPalette.GHOST_WHITE,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    padding: '2vh',
+    height: '90vh',
+    width: '85vw'
+  },
+  header: {
+    fontSize: '1vh'
+  },
+  socialContent: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly'
+  },
+  smallCard: {
+    width: '35vh'
+  },
+  media: {
+    height: 0,
+    paddingTop: '100%'
   }
 });
-
-const StyledGrid = styled(Grid)({
-  height: '100%',
-  width: '100%',
-  display: 'flex',
-  alignItems: 'center',
-  flexDirection: 'column',
-  justifyContent: 'center'
-});
-
 
 const AboutMe = () => {
   const classes = styles();
 
   return (
-    <StyledGrid>
+    <Grid className={ classes.grid }>
       <Paper className={ classes.section }>
-        <Avatar alt="picture of Sam Bley" src="/assets/selfie.jpg" className={ classes.avatar } />
-        <Typography varient="p">
-          Lorem ipsum dolor sit amet, nonumy euismod ad eam, vis ut sonet detraxit,
-          no virtute deterruisset sea.
-          Oporteat quaestio nam ne, mucius molestiae vix at.
-          Te pro quod eros timeam. Ut dicat incorrupte referrentur quo. Ei pro legimus accusam.
-        </Typography>
+        <Card className={ classes.smallCard }>
+          <CardHeader
+            title="Connect With Me On"
+            className={ classes.header }
+          />
+          <CardMedia
+            title="that's me"
+            image="/assets/selfie.jpg"
+            className={ classes.media }
+          />
+          <CardContent className={ classes.socialContent }>
+            <a href="https://github.com/samibanley">{socialMedia.github}</a>
+            <a href="https://www.linkedin.com/in/samibanley/">{socialMedia.linkedIn}</a>
+            <a href="https://twitter.com/samibanley">{ socialMedia.twitter }</a>
+          </CardContent>
+        </Card>
+        <Card className={ classes.smallCard }>
+          <CardHeader
+            title={ interests.sectionTitle }
+            className={ classes.header }
+          />
+          <CardContent className={ classes.socialContent }>
+            <a href="https://github.com/samibanley">{socialMedia.github}</a>
+            <a href="https://www.linkedin.com/in/samibanley/">{socialMedia.linkedIn}</a>
+            <a href="https://twitter.com/samibanley">{ socialMedia.twitter }</a>
+          </CardContent>
+        </Card>
       </Paper>
-    </StyledGrid>
+    </Grid>
   );
 };
 
